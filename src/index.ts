5b24425403522caca96800d908254d3818155971
nanosoftdev <devnano@nanosoftsolutions.in>
2025-01-02 09:44:52 +0530

    default export removed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Toast, { positionType } from "./Components/Toast";
 import { ToastProvider, useToast, type ToastOptions, type ToastContextType } from "./Context/ToastProvider";
 
-export default Toast;
 export { positionType, ToastProvider, useToast, ToastOptions, ToastContextType };
